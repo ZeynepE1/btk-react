eddc82db56fc00bc123b059364851f70cb491b55
ZeynepE1 <81360699+ZeynepE1@users.noreply.github.com>
2021-10-28 10:34:50 +0300

    components

---
+++ redux-intro/src/components/IncreaseByTwoCoubter.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class IncreaseByTwoCoubter extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ redux-intro/src/components/IncreaseCounter.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class IncreaseCounter extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
