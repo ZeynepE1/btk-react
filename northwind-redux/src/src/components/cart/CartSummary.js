35f5a1b40b64de55210ed38dc3c71e7a3a4a9a19
ZeynepE1 <81360699+ZeynepE1@users.noreply.github.com>
2021-11-02 13:16:01 +0300

    Add files via upload

---
+++ northwind-redux/src/src/components/cart/CartSummary.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class CartSummary extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
