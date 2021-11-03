72e093a2bb0dcc710a7aec398435dd8dfa5173af
ZeynepE1 <81360699+ZeynepE1@users.noreply.github.com>
2021-11-03 14:48:52 +0300

    Add files via upload

--- northwind-redux/src/components/products/ProductList.js
+++ northwind-redux/src/components/products/ProductList.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Badge } from "reactstrap";
+import { Badge, Table } from "reactstrap";
 import * as productActions from "../../redux/actions/productActions";
 import { bindActionCreators } from "redux";
 
-
 class ProductList extends Component {
-
   componentDidMount() {
     this.props.actions.getProducts();
   }
@@ -14,12 +12,33 @@ class ProductList extends Component {
     return (
       <div>
         <h3>
-          <Badge color="warning">
-            Products
-          </Badge>
+          <Badge color="warning">Products</Badge>
           <Badge color="success">
             {this.props.currentCategory.categoryName}
           </Badge>
+          <Table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Product Name</th>
+                <th>Unit Price</th>
+                <th>Quantity Per Unit</th>
+                <th>Units in Stock</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.products.map((product) => (
+                <tr>
+                  <th scope="row">{product.id}</th>
+                  <td>{product.productName}</td>
+                  <td>{product.unitPrice}</td>
+                  <td>{product.quantityPerUnit}</td>
+                  <td>{product.unitsInStock}</td>
+
+                </tr>
+              ))}
+            </tbody>
+          </Table>
         </h3>
       </div>
     );
@@ -29,19 +48,16 @@ class ProductList extends Component {
 function mapStateToProps(state) {
   return {
     currentCategory: state.changeCategoryReducer,
-    products:state.productListReducer,
+    products: state.productListReducer,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
     actions: {
-      getProducts: bindActionCreators(
-        productActions.getProducts,
-        dispatch
-      ),
+      getProducts: bindActionCreators(productActions.getProducts, dispatch),
     },
   };
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(ProductList);
+export default connect(mapStateToProps, mapDispatchToProps)(ProductList);
